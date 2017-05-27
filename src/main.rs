20185315b2e162710415995bdf36af712b9504f6
Daniel Hauck <mail@dhauck.eu>
2017-05-27 16:00:42 +0200

    Add better image.

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let nthreads = 300;
+    let nthreads = 10;
     let image = open("image.png").expect("image").to_rgba();
 
     loop {
-        let x0 = 1700;
-        let y0 = 700;
+        let x0 = 300;
+        let y0 = 1200-image.height();
         let mut handles = Vec::new();
         println!("→ {} {}", x0, y0);
-        for xoff in 0..nthreads {
+        for n in 0..nthreads {
             let img = image.clone();
+            let xoff = n*image.width()/nthreads;
             handles.push(thread::spawn(move || {
                 loop {
                     match TcpStream::connect("94.45.231.39:1234") {
@@ -27,7 +28,7 @@ fn main() {
                             loop {
                                 y = (y+1) % img.height();
                                 if y == 0 {
-                                    x = (x+xoff) % img.width();
+                                    x = (x+1) % img.width();
                                 }
                                 let (r, g, b, a) = img.get_pixel(x, y).channels4();
                                 if a > 10 {
